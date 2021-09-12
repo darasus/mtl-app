2e29681cf47a549af0e9fe7ebefce9820e83b0f2
Ilya Daraseliya <ilya.daraseliya@leaseplan.com>
2021-09-12 11:02:05 +0200

    fix: auth

--- hooks/useMeQuery.ts
+++ hooks/useMeQuery.ts
@@ -1,8 +1,12 @@
+import { useSession } from "next-auth/client";
 import { useQuery } from "react-query";
 import { fetchMe } from "../request/fetchMe";
 
 export const useMeQuery = () => {
+  const [session] = useSession();
+
   return useQuery("me", fetchMe, {
     staleTime: 1000 * 60 * 5,
+    enabled: !!session,
   });
 };
--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 const withPlugins = require("next-compose-plugins");
 
 module.exports = withPlugins([], {
-  webpack5: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-  env: {
-    // app
-    BASE_URL: process.env.VERCEL_URL,
-    // prisma
-    DATABASE_URL:
-      "mysql://doadmin:ix2s813ci20zmr3h@db-mysql-ams3-57417-do-user-1778867-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED",
-    // auth
-    GITHUB_ID: "6bd2ff63e6651b8196a1",
-    GITHUB_SECRET: "950dd24bd72eff809a44b0863ce117f94e7602f1",
-    NEXTAUTH_URL: process.env.VERCEL_URL,
-  },
+  webpack5: true,
+  // env: {
+  //   // app
+  //   BASE_URL: process.env.VERCEL_URL,
+  //   // prisma
+  //   DATABASE_URL:
+  //     "mysql://doadmin:ix2s813ci20zmr3h@db-mysql-ams3-57417-do-user-1778867-0.b.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED",
+  //   // auth
+  //   GITHUB_ID: "6bd2ff63e6651b8196a1",
+  //   GITHUB_SECRET: "950dd24bd72eff809a44b0863ce117f94e7602f1",
+  //   NEXTAUTH_URL: process.env.VERCEL_URL,
+  // },
 });
