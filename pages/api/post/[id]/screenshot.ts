ae3951ba5c6d5000e7929dd11bc98416c82612f0
Ilya Daraseliya <ilya.daraseliya@leaseplan.com>
2021-09-21 07:51:57 +0200

    fix: screenshot api

--- pages/api/post/[id]/screenshot.ts
+++ pages/api/post/[id]/screenshot.ts
@@ -13,21 +13,17 @@ export default async function handle(
 
   let browser = null;
 
-  try {
-    browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1200, height: 630 });
-    await page.goto(`http://localhost:3000/p/${req.query.id}/preview`);
-    await page.waitForSelector('[data-testid="post-title"]');
-    await page.waitFor(100);
-    const screenshot = await page.screenshot({
-      type: "png",
-      encoding: "binary",
-    });
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "image/png");
-    return res.end(screenshot);
-  } catch (error) {
-    return res.status(500).send(error);
-  }
+  browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1200, height: 630 });
+  await page.goto(`http://localhost:3000/p/${req.query.id}/preview`);
+  await page.waitForSelector('[data-testid="post-title"]');
+  await page.waitFor(1000);
+  const screenshot = await page.screenshot({
+    type: "png",
+    encoding: "binary",
+  });
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "image/png");
+  return res.end(screenshot);
 }
