9461895f4c1e829e88f5e414e51a3e571a4b2ec0
Ilya Daraseliya <idarase@gmail.com>
2021-10-07 07:51:57 +0200

    chore: setup dd rum

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,9 +14,37 @@ class MyDocument extends Document {
   }
 
   render() {
+    const config = JSON.stringify({
+      applicationId: "2cc395d9-aead-4bfd-95d7-29a54f536d42",
+      clientToken: "pub3033d36d5b288f88e5708e459e43ed52",
+      site: "datadoghq.eu",
+      service: "my-little-library",
+      env: process.env.ENVIRONMENT,
+      resourceSampleRate: 100,
+      // Specify a version number to identify the deployed version of your application in Datadog
+      version: process.env.VERCEL_GIT_COMMIT_SHA,
+      sampleRate: 100,
+      trackInteractions: true,
+      allowedTracingOrigins: [process.env.NEXTAUTH_URL],
+    });
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {process.env.VERCEL_ENV === "production" && (
+            <>
+              <script
+                src="https://www.datadoghq-browser-agent.com/datadog-rum-v3.js"
+                type="text/javascript"
+              ></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.DD_RUM && window.DD_RUM.init(${config});`,
+                }}
+              ></script>
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
