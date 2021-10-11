4eaa003dfb74a21b9acf1faa39a96627d487cb4d
Ilya Daraseliya <idarase@gmail.com>
2021-10-11 17:59:00 +0200

    fix: screenshot size

--- layouts/PreviewLayout.tsx
+++ layouts/PreviewLayout.tsx
@@ -9,9 +9,10 @@ export const PreviewLayout: React.FC<Props> = (props) => (
   <>
     <Flex
       width="100vw"
-      height="100vh"
       justifyContent="center"
       alignItems="center"
+      mt={20}
+      mb={20}
     >
       <Flex flexDirection="column" width="100%" maxWidth={960} flexShrink={0}>
         <Box marginBottom="size-200">{props.children}</Box>
--- pages/api/post/[id]/screenshot.ts
+++ pages/api/post/[id]/screenshot.ts
@@ -12,20 +12,21 @@ export default async function handle(
     req.method === "GET",
     `The HTTP ${req.method} method is not supported at this route.`
   );
-  const isLocalhost = req.headers.host === "localhost:3000";
 
-  let browser = null;
-  browser = await (isLocalhost ? puppeteer : puppeteerCore).launch({
+  const isLocalhost = req.headers.host === "localhost:3000";
+  const browser = await (isLocalhost ? puppeteer : puppeteerCore).launch({
     headless: true,
     ...(isLocalhost
       ? {}
       : { args: chrome.args, executablePath: await chrome.executablePath }),
   });
   const page = await browser.newPage();
+
   await page.setViewport({
     width: Number(req.query.width) || 1200,
     height: Number(req.query.height) || 630,
   });
+
   await page.goto(
     `${process.env.NEXTAUTH_URL}/p/${req.query.id}/preview`,
     isLocalhost
@@ -36,10 +37,14 @@ export default async function handle(
           waitUntil: "networkidle0",
         }
   );
+
   const screenshot = await page.screenshot({
     type: "png",
     encoding: "binary",
+    captureBeyondViewport: true,
+    fullPage: true,
   });
+
   res.statusCode = 200;
   return res.end(screenshot);
 }
