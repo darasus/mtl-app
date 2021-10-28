a37b3df5e6de79be895471733f098bca96e8c120
Ilya Daraseliya <idarase@gmail.com>
2021-10-28 11:26:38 +0200

    chore: prisma log middleware

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -22,4 +22,18 @@ if (process.env.NODE_ENV === "production") {
   prisma = (global as any).prisma;
 }
 
+prisma.$use(async (params, next) => {
+  const before = Date.now();
+
+  const result = await next(params);
+
+  const after = Date.now();
+
+  console.log(
+    `Query ${params.model}.${params.action} took ${after - before}ms`
+  );
+
+  return result;
+});
+
 export default prisma;
