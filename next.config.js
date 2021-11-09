e93993c9831e9c6c72455e70ec411f77e275eddc
Ilya Daraseliya <idarase@gmail.com>
2021-11-09 20:47:35 +0100

    fix: vercel url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withPlugins = require("next-compose-plugins");
 
 const baseUrl =
-  process.env.VERCEL_URL === "localhost:3000"
+  process.env.VERCEL_ENV === "production"
+    ? "https://www.mytinylibrary.com/"
+    : process.env.VERCEL_URL === "localhost:3000"
     ? `http://${process.env.VERCEL_URL}`
     : `https://${process.env.VERCEL_URL}`;
 
