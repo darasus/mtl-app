ab1958db100414aaa1bd6e87d893f3f9fc94cbe7
Ilya Daraseliya <idarase@gmail.com>
2021-11-13 09:12:45 +0100

    debug auth update

--- pages/api/user/[id]/update-settings.ts
+++ pages/api/user/[id]/update-settings.ts
@@ -1,4 +1,5 @@
 import { withApiAuthRequired } from "@auth0/nextjs-auth0";
+import axios from "axios";
 import invariant from "invariant";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getUserSession } from "../../../../lib/getUserSession";
@@ -18,6 +19,51 @@ export default withApiAuthRequired(async function handle(
   const userService = new UserService();
 
   try {
+    const token = await axios(`https://mtl-app.eu.auth0.com/oauth/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        grant_type: "client_credentials",
+        client_id: process.env.AUTH0_CLIENT_ID,
+        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        audience: `https://mtl-app.eu.auth0.com/api/v2/`,
+      },
+    })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        return res.status(400).end(processErrorResponse(err));
+      });
+
+    const session = await getUserSession({ req, res });
+
+    await axios(
+      `https://mtl-app.eu.auth0.com/api/v2/users/${session.user.sub}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token.access_token}`,
+          "Content-Type": "application/json",
+        },
+        data: {
+          picture: req.body.image,
+          name: req.body.name,
+          nickname: req.body.nickname,
+          password: req.body.password,
+          email: req.body.email,
+        },
+      }
+    )
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        return res.status(400).end(processErrorResponse(err));
+      });
+
     await userService.updateUserSettings({
       userId: req.query.id,
       image: req.body.image,
@@ -26,44 +72,6 @@ export default withApiAuthRequired(async function handle(
       password: req.body.password,
       email: req.body.email,
     });
-
-    const token = await fetch(
-      `${process.env.AUTH0_ISSUER_BASE_URL}/oauth/token`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          grant_type: "client_credentials",
-          client_id: process.env.AUTH0_CLIENT_ID,
-          client_secret: process.env.AUTH0_CLIENT_SECRET,
-          audience: `${process.env.AUTH0_ISSUER_BASE_URL}/api/v2/`,
-        }),
-      }
-    ).then((res) => res.json());
-
-    const session = await getUserSession({ req, res });
-
-    const res2 = await fetch(
-      `${process.env.AUTH0_ISSUER_BASE_URL}/api/v2/users/${session.user.sub}`,
-      {
-        method: "PATCH",
-        headers: {
-          Authorization: `Bearer ${token.access_token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          picture: req.body.image,
-          name: req.body.name,
-          nickname: req.body.nickname,
-          password: req.body.password,
-          email: req.body.email,
-        }),
-      }
-    ).then((res) => res.json());
-
-    console.log({ res2 });
 
     return res.json({});
   } catch (error) {
